6c329a12e0875ce8c83f03446956f1969b7cc302
jatin-rajput563 <jkt3298@gmail.com>
2025-06-07 16:51:16 +0530

    work-section-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Work from "./components/Work";
 
 function App() {
   return (
     <>
       <Hero />
+      <Work />
     </>
   );
 }
